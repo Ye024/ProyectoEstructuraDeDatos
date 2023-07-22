551b4dc18e243891eefe320c9e9c6bddad876f98
aricastr <93687988+aricastr@users.noreply.github.com>
2023-07-21 19:24:26 -0600

    Update Ingredient.java
    Primer codigo de clase ingrediente.

--- src/overcookedfide/Ingredient.java
+++ src/overcookedfide/Ingredient.java
@@ -10,5 +10,48 @@ package overcookedfide;
  * @author admin
  */
 public class Ingredient {
-    
+    String tomate;
+    String lechuga;
+    String carne;
+    String pan;
+
+    public Persona1(String tomate, String lechuga, String carne, String pan) {
+        this.tomate = tomate;
+        this.lechuga = lechuga;
+        this.carne = carne;
+        this.pan = pan;
+    }
+
+    public String getTomate() {
+        return tomate;
+    }
+
+    public void setTomate(String tomate) {
+        this.tomate = tomate;
+    }
+
+    public String getLechuga() {
+        return lechuga;
+    }
+
+    public void setLechuga(String lechuga) {
+        this.lechuga = lechuga;
+    }
+
+    public String getCarne() {
+        return carne;
+    }
+
+    public void setCarne(String carne) {
+        this.carne = carne;
+    }
+
+    public String getPan() {
+        return pan;
+    }
+
+    public void setPan(String pan) {
+        this.pan = pan;
+    }
+
 }
