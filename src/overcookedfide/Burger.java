3a4060160ba1d9ee7c648310edef9de7c8075ed7
aricastr <93687988+aricastr@users.noreply.github.com>
2023-07-21 19:30:28 -0600

    Update Burger.java
    Primer codigo clase burger.

--- src/overcookedfide/Burger.java
+++ src/overcookedfide/Burger.java
@@ -10,5 +10,37 @@ package overcookedfide;
  * @author admin
  */
 public class Burger {
-    
+    String ham_carne;
+    String ham_queso;
+    String ham_clasica;
+
+    public Persona1(String ham_carne, String ham_queso, String ham_clasica) {
+        this.ham_carne = ham_carne;
+        this.ham_queso = ham_queso;
+        this.ham_clasica = ham_clasica;
+    }
+
+    public String getHam_carne() {
+        return ham_carne;
+    }
+
+    public void setHam_carne(String ham_carne) {
+        this.ham_carne = ham_carne;
+    }
+
+    public String getHam_queso() {
+        return ham_queso;
+    }
+
+    public void setHam_queso(String ham_queso) {
+        this.ham_queso = ham_queso;
+    }
+
+    public String getHam_clasica() {
+        return ham_clasica;
+    }
+
+    public void setHam_clasica(String ham_clasica) {
+        this.ham_clasica = ham_clasica;
+    }
 }
