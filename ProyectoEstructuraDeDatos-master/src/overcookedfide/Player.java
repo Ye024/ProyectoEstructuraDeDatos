e96d501312dbc5a23b4e61cc4ba249004f18b0d5
aricastr <93687988+aricastr@users.noreply.github.com>
2023-08-14 14:58:33 -0600

    Update Player.java
    Establecido valor por defecto del Puntaje

--- ProyectoEstructuraDeDatos-master/src/overcookedfide/Player.java
+++ ProyectoEstructuraDeDatos-master/src/overcookedfide/Player.java
@@ -15,7 +15,7 @@ public class Player {
 
     public Player(String nombre, int puntaje) {
         this.nombre = nombre;
-        this.puntaje = puntaje;
+        this.puntaje = 0;
     }
 
     public String getNombre() {
